4a00a3bdf74728a92b81ae001662ad83bf13d9f4
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-04-24 14:16:37 +0300

    update script

--- test-with-j2eo.sh
+++ test-with-j2eo.sh
@@ -7,6 +7,6 @@ cd ..
 git clone https://github.com/br4ch1st0chr0n3/j2eo || true
 cd j2eo
 ./test-hadoop.sh
-cd ../polyscat
+cd ../polystat-cli
 sbt assembly
-java -jar target/scala-2.13/polyscat.jar --files ../j2eo_hadoop/ --out ../polyscat_hadoop
+java -jar target/scala-2.13/polystat.jar --files ../j2eo_hadoop/ --out ../polystat_hadoop
