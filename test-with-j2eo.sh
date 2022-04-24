8c517f7ac4d96be99054a9db062abc241e8151db
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-04-24 15:47:16 +0300

    fix script to test with odin branch introducing_suuport_for_aliases

--- test-with-j2eo.sh
+++ test-with-j2eo.sh
@@ -4,9 +4,16 @@
 set -e  
 
 cd ..
+mkdir polystat-data
+cd polystat-data
 git clone https://github.com/br4ch1st0chr0n3/j2eo || true
 cd j2eo
 ./test-hadoop.sh
-cd ../polystat-cli
+cd ..
+git clone https://github.com/br4ch1st0chr0n3/odin
+cd odin
+git checkout introducing_suuport_for_aliases
+sbt publishLocal
+cd ../../polystat-cli
 sbt assembly
-java -jar target/scala-2.13/polystat.jar --files ../j2eo_hadoop/ --out ../polystat_hadoop
+java -jar target/scala-2.13/polystat.jar --files ../polystat-data/j2eo-data/hadoop-eo --out ../polystat-data/hadoop-polystat
