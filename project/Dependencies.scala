01cb8281b6d117b0298674011c633e4d8282c51a
nikololiahim <olomishcak@outlook.com>
2022-09-04 17:03:49 +0300

    updated j2eo and py2eo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 object Dependencies {
   object V {
     val far = "0.2.0"
-    val j2eo = "0.5.3"
+    val j2eo = "0.6.0"
     val odin = "0.4.5"
-    val py2eo = "0.0.14"
+    val py2eo = "0.23.0"
   }
 }
